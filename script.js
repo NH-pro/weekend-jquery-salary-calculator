d129de208cd279dd2cc0d6ff011d12b22c94f52f
Neil Hanson <pro.neilhanson@gmail.com>
2022-05-13 16:57:44 -0500

    created addEmployee function that can append to the DOM

--- script.js
+++ script.js
@@ -2,4 +2,34 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('jq is sourced');
+
+    $('#submit_button').on('click', addEmployee);
 }
+// END 'onReady' function.
+
+function addEmployee() {
+    let firstName = $('#input_first_name').val();
+    let lastName = $('#input_last_name').val();
+    let idNumber = $('#input_id_number').val();
+    let jobTitle = $('#input_job_title').val();
+    let annualSalary = $('#input_annual_salary').val();
+    // Create employee info vars and set their value = to their employee input field value.
+
+    $('#employee_table').append(`
+        <tr>
+           <td>${firstName}</td>
+           <td>${lastName}</td>
+           <td>${idNumber}</td>
+           <td>${jobTitle}</td>
+           <td>${annualSalary}</td>
+        `);
+    // Select 'employee_table' and append employee info vars in a new <tr> tag.
+    
+    $('#input_first_name').val(null);
+    $('#input_last_name').val(null);
+    $('#input_id_number').val(null);
+    $('#input_job_title').val(null);
+    $('#input_annual_salary').val(null);
+    // Set input field values to null.
+}
+// END 'addEmployee' function.
